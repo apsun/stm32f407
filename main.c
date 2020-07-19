efeae65e87178274398fab4e4247606b77f0338e
Andrew Sun <andrew@crossbowffs.com>
2020-07-18 19:58:51 -0700

    Add support for button interrupts

--- main.c
+++ main.c
@@ -2,66 +2,150 @@
 #include "stdbool.h"
 #include "mmio.h"
 
+#define GPIO_PIN_BUTTON 0
 #define GPIO_PIN_LED3 13
 #define GPIO_PIN_LED4 12
 #define GPIO_PIN_LED5 14
 #define GPIO_PIN_LED6 15
 
-void gpio_clock_on(void) {
-    *(reg_t *)RCC_AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-    (void)*(reg_t *)RCC_AHB1ENR;
+#define IRQ_EXTI0 6
+
+static volatile int pattern = 0;
+
+static void clock_enable(reg_t *clock, uint32_t bits) {
+    *clock |= bits;
+    (void)*clock;
 }
 
-void gpio_set(int pin, bool on) {
-    *(reg_t *)GPIOD_BSRR = ((1U << pin) << (on ? 0 : 16));
+static void led_init_gpio(int pin) {
+    GPIO_OSPEEDR(GPIOD_BASE) |= GPIO_OSPEEDR_HI << (pin * 2);
+    GPIO_OTYPER(GPIOD_BASE) |= GPIO_OTYPER_PP << pin;
+    GPIO_PUPDR(GPIOD_BASE) |= GPIO_PUPDR_PU << (pin * 2);
+    GPIO_MODER(GPIOD_BASE) |= GPIO_MODER_OUTPUT << (pin * 2);
 }
 
-bool gpio_get(int pin) {
-    return (*(reg_t *)GPIOD_ODR & (1U << pin)) == (1U << pin);
+static void led_init(void) {
+    clock_enable(&RCC_AHB1ENR, RCC_AHB1ENR_GPIODEN);
+    led_init_gpio(GPIO_PIN_LED3);
+    led_init_gpio(GPIO_PIN_LED4);
+    led_init_gpio(GPIO_PIN_LED5);
+    led_init_gpio(GPIO_PIN_LED6);
 }
 
-void gpio_init_for_led(int pin) {
-    *(reg_t *)GPIOD_OSPEEDR |= GPIOD_OSPEEDR_HI << (pin * 2);
-    *(reg_t *)GPIOD_OTYPER |= GPIOD_OTYPER_PP << pin;
-    *(reg_t *)GPIOD_PUPDR |= GPIOD_PUPDR_PU << (pin * 2);
-    *(reg_t *)GPIOD_MODER |= GPIOD_MODER_OUTPUT << (pin * 2);
+static void led_set(int pin, bool on) {
+    GPIO_BSRR(GPIOD_BASE) = ((1U << pin) << (on ? 0 : 16));
 }
 
-void delay(void) {
+static void button_init_gpio(void) {
+    clock_enable(&RCC_AHB1ENR, RCC_AHB1ENR_GPIOAEN);
+    GPIO_OSPEEDR(GPIOA_BASE) |= GPIO_OSPEEDR_HI << (GPIO_PIN_BUTTON * 2);
+    GPIO_OTYPER(GPIOA_BASE) |= GPIO_OTYPER_PP << GPIO_PIN_BUTTON;
+    GPIO_PUPDR(GPIOA_BASE) |= GPIO_PUPDR_NONE << (GPIO_PIN_BUTTON * 2);
+    GPIO_MODER(GPIOA_BASE) |= GPIO_MODER_INPUT << (GPIO_PIN_BUTTON * 2);
+}
+
+static void exti_map_gpio(int exti, int port) {
+    static reg_t * const regs[] = {
+        &SYSCFG_EXTICR1,
+        &SYSCFG_EXTICR2,
+        &SYSCFG_EXTICR3,
+        &SYSCFG_EXTICR4,
+    };
+
+    // System configuration controller is needed to manage EXTI so we
+    // have to enable its clock here
+    clock_enable(&RCC_APB2ENR, RCC_APB2ENR_SYSCFGEN);
+
+    // EXTICR1 contains EXTI0-EXTI3, EXTICR2 contains EXTI4-EXTI7, etc.
+    // Each register is 4 lines * 4 bits each, bits are 0 for port A,
+    // 1 for port B, etc.
+    *regs[exti / 4] |= (port << (4 * (exti % 4)));
+}
+
+static void exti_clear_irq(int exti) {
+    EXTI_PR = (1U << exti);
+}
+
+static void button_init_exti(void) {
+    // Connect GPIO port A to EXTI0
+    exti_map_gpio(0 /* EXTI0 */, 0 /* GPIOA */);
+
+    // Interrupt mode, raise on rising edge
+    EXTI_IMR |= (1U << GPIO_PIN_BUTTON);
+    EXTI_RTSR |= (1U << GPIO_PIN_BUTTON);
+}
+
+static void button_init_nvic(void) {
+    NVIC_ISER(IRQ_EXTI0 / 32) = (1 << IRQ_EXTI0);
+}
+
+static void button_init(bool interrupts) {
+    button_init_gpio();
+    if (interrupts) {
+        button_init_exti();
+        button_init_nvic();
+    }
+}
+
+static bool button_get(void) {
+    return (GPIO_IDR(GPIOA_BASE) & (1U << GPIO_PIN_BUTTON)) == (1U << GPIO_PIN_BUTTON);
+}
+
+static void delay(void) {
     for (int i = 0; i < 200000; ++i) {
         __asm volatile("nop");
     }
 }
 
+static void handle_irq_button(void) {
+    pattern = (pattern + 1) % 3;
+    exti_clear_irq(GPIO_PIN_BUTTON);
+}
+
+void exception_handler(int n) {
+    if (n <= 6) {
+        while (1);
+    }
+
+    if (n == 16 + IRQ_EXTI0) {
+        handle_irq_button();
+    }
+}
+
 int main(void) {
-    gpio_clock_on();
-    gpio_init_for_led(GPIO_PIN_LED3);
-    gpio_init_for_led(GPIO_PIN_LED4);
-    gpio_init_for_led(GPIO_PIN_LED5);
-    gpio_init_for_led(GPIO_PIN_LED6);
-    while (1) {
-        for (int i = 0; i < 16; ++i) {
-            gpio_set(GPIO_PIN_LED3, (i + 0) % 4 == 0);
-            gpio_set(GPIO_PIN_LED4, (i + 1) % 4 == 0);
-            gpio_set(GPIO_PIN_LED6, (i + 2) % 4 == 0);
-            gpio_set(GPIO_PIN_LED5, (i + 3) % 4 == 0);
-            delay();
+    led_init();
+    button_init(true);
+
+    for (int i = 0; ; i++) {
+        if (button_get()) {
+            led_set(GPIO_PIN_LED3, true);
+            led_set(GPIO_PIN_LED4, true);
+            led_set(GPIO_PIN_LED6, true);
+            led_set(GPIO_PIN_LED5, true);
+            continue;
         }
 
-        for (int i = 0; i < 16; ++i) {
-            gpio_set(GPIO_PIN_LED3, (i + 0) % 8 >= 4);
-            gpio_set(GPIO_PIN_LED4, (i + 1) % 8 >= 4);
-            gpio_set(GPIO_PIN_LED6, (i + 2) % 8 >= 4);
-            gpio_set(GPIO_PIN_LED5, (i + 3) % 8 >= 4);
-            delay();
+        switch (pattern) {
+        case 0:
+            led_set(GPIO_PIN_LED3, (i + 0) % 4 == 0);
+            led_set(GPIO_PIN_LED4, (i + 1) % 4 == 0);
+            led_set(GPIO_PIN_LED6, (i + 2) % 4 == 0);
+            led_set(GPIO_PIN_LED5, (i + 3) % 4 == 0);
+            break;
+        case 1:
+            led_set(GPIO_PIN_LED3, (i + 0) % 8 >= 4);
+            led_set(GPIO_PIN_LED4, (i + 1) % 8 >= 4);
+            led_set(GPIO_PIN_LED6, (i + 2) % 8 >= 4);
+            led_set(GPIO_PIN_LED5, (i + 3) % 8 >= 4);
+            break;
+        case 2:
+            led_set(GPIO_PIN_LED3, i % 2 == 0);
+            led_set(GPIO_PIN_LED4, i % 2 == 1);
+            led_set(GPIO_PIN_LED6, i % 2 == 0);
+            led_set(GPIO_PIN_LED5, i % 2 == 1);
+            break;
         }
 
-        for (int i = 0; i < 16; ++i) {
-            gpio_set(GPIO_PIN_LED3, i % 2 == 0);
-            gpio_set(GPIO_PIN_LED4, i % 2 == 1);
-            gpio_set(GPIO_PIN_LED6, i % 2 == 0);
-            gpio_set(GPIO_PIN_LED5, i % 2 == 1);
-            delay();
-        }
+        delay();
     }
 }
--- mmio.h
+++ mmio.h
@@ -3,30 +3,56 @@
 
 typedef volatile uint32_t reg_t;
 
+#define REG(addr) (*(reg_t *)(addr))
+
+#define SCS_BASE                        0xE000E000UL
+  #define NVIC_BASE                     (SCS_BASE + 0x100UL)
+    #define NVIC_ISER(n)                REG(NVIC_BASE + 0x000UL + ((n) * 4))
+    #define NVIC_ICER(n)                REG(NVIC_BASE + 0x080UL + ((n) * 4))
+    #define NVIC_ISPR(n)                REG(NVIC_BASE + 0x100UL + ((n) * 4))
+    #define NVIC_ICPR(n)                REG(NVIC_BASE + 0x180UL + ((n) * 4))
+    #define NVIC_IABR(n)                REG(NVIC_BASE + 0x200UL + ((n) * 4))
+
 #define PERIPH_BASE                     0x40000000UL
+  #define APB2PERIPH_BASE               (PERIPH_BASE + 0x00010000UL)
+    #define SYSCFG_BASE                 (APB2PERIPH_BASE + 0x3800UL)
+      #define SYSCFG_EXTICR1            REG(SYSCFG_BASE + 0x08UL)
+      #define SYSCFG_EXTICR2            REG(SYSCFG_BASE + 0x0CUL)
+      #define SYSCFG_EXTICR3            REG(SYSCFG_BASE + 0x10UL)
+      #define SYSCFG_EXTICR4            REG(SYSCFG_BASE + 0x04UL)
+    #define EXTI_BASE                   (APB2PERIPH_BASE + 0x3C00UL)
+      #define EXTI_IMR                  REG(EXTI_BASE + 0x00UL)
+      #define EXTI_RTSR                 REG(EXTI_BASE + 0x08UL)
+      #define EXTI_FTSR                 REG(EXTI_BASE + 0x0CUL)
+      #define EXTI_PR                   REG(EXTI_BASE + 0x14UL)
   #define AHB1PERIPH_BASE               (PERIPH_BASE + 0x00020000UL)
     #define RCC_BASE                    (AHB1PERIPH_BASE + 0x3800UL)
-      #define RCC_AHB1ENR               (RCC_BASE + 0x30UL)
+      #define RCC_AHB1ENR               REG(RCC_BASE + 0x30UL)
+        #define RCC_AHB1ENR_GPIOAEN     (1 << 0)
         #define RCC_AHB1ENR_GPIODEN     (1 << 3)
+      #define RCC_APB2ENR               REG(RCC_BASE + 0x44UL)
+        #define RCC_APB2ENR_SYSCFGEN    (1 << 14)
+    #define GPIOA_BASE                  (AHB1PERIPH_BASE + 0x0000UL)
     #define GPIOD_BASE                  (AHB1PERIPH_BASE + 0x0C00UL)
-      #define GPIOD_OSPEEDR             (GPIOD_BASE + 0x08UL)
-        #define GPIOD_OSPEEDR_LOW       0x0
-        #define GPIOD_OSPEEDR_MED       0x1
-        #define GPIOD_OSPEEDR_HI        0x2
-        #define GPIOD_OSPEEDR_VERYHI    0x3
-      #define GPIOD_OTYPER              (GPIOD_BASE + 0x04UL)
-        #define GPIOD_OTYPER_PP         0x0
-        #define GPIOD_OTYPER_OD         0x1
-      #define GPIOD_PUPDR               (GPIOD_BASE + 0x0CUL)
-        #define GPIOD_PUPDR_NONE        0x0
-        #define GPIOD_PUPDR_PU          0x1
-        #define GPIOD_PUPDR_PD          0x2
-      #define GPIOD_MODER               (GPIOD_BASE + 0x00UL)
-        #define GPIOD_MODER_INPUT       0x0
-        #define GPIOD_MODER_OUTPUT      0x1
-        #define GPIOD_MODER_ALT         0x2
-        #define GPIOD_MODER_ANALOG      0x3
-      #define GPIOD_BSRR                (GPIOD_BASE + 0x18UL)
-      #define GPIOD_ODR                 (GPIOD_BASE + 0x14UL)
+      #define GPIO_OSPEEDR(base)        REG((base) + 0x08UL)
+        #define GPIO_OSPEEDR_LOW        0x0
+        #define GPIO_OSPEEDR_MED        0x1
+        #define GPIO_OSPEEDR_HI         0x2
+        #define GPIO_OSPEEDR_VERYHI     0x3
+      #define GPIO_OTYPER(base)         REG((base) + 0x04UL)
+        #define GPIO_OTYPER_PP          0x0
+        #define GPIO_OTYPER_OD          0x1
+      #define GPIO_PUPDR(base)          REG((base) + 0x0CUL)
+        #define GPIO_PUPDR_NONE         0x0
+        #define GPIO_PUPDR_PU           0x1
+        #define GPIO_PUPDR_PD           0x2
+      #define GPIO_MODER(base)          REG((base) + 0x00UL)
+        #define GPIO_MODER_INPUT        0x0
+        #define GPIO_MODER_OUTPUT       0x1
+        #define GPIO_MODER_ALT          0x2
+        #define GPIO_MODER_ANALOG       0x3
+      #define GPIO_IDR(base)            REG((base) + 0x10UL)
+      #define GPIO_ODR(base)            REG((base) + 0x14UL)
+      #define GPIO_BSRR(base)           REG((base) + 0x18UL)
 
 #endif
